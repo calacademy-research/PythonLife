6153150422f576517445d6423322321e53d76bc0
Joe Russack <joe@nothingugly.com>
2018-04-12 14:59:17 -0700

    Who doesn't like ice cream. nobody, that's who.

---
+++ Subject_7_running_in_the_real_world/bad_ice_cream.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+def favorite_ice_cream(n):
+    
+    ice_creams = [
+        "chocolate",
+        "vanilla",
+        "strawberry"
+    ]
+    print(ice_creams[n])
+
+favorite_ice_cream(99)
+
